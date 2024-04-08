bfb641a32aa06c46e7debbc92673ccff6600e96b
eyemkey <armanmkrtchyan100101@gmail.com>
2024-04-08 01:32:44 -0700

    Sorting, Removal, More Info

---
+++ titles.js
@@ -0,0 +1,10 @@
+let titles = [
+    {title: "God of War", price: 19.99, platforms: ["PS4", "PC"], metacriticScore: 94, genres: ["Action"], publisher: "Sony Interactive Entertainment", developer: "SCE Santa Monica", releaseDate: "04/20/2018", imagePath: "./images/god-of-war.jpg"},
+    {title: "Marvel's Spider-Man Remastered", price: 49.99, platforms: ["PS5","PS4", "PC"], metacriticScore: 87, genres: ["Action"], publisher: "Playstation PC", developer: "Nixxes Software", releaseDate: "08/12/2022", imagePath: "./images/spider-man.jpg" },
+    {title: "Uncharted 4: A Thief's End", price: 19.99, platforms: ["PS4", "PC"], metacriticScore: 93, genres: ["Action"], publisher: "Sony Interactive Entertainment", developer: "Naughty Dog", releaseDate: "05/10/2016", imagePath: "./images/uncharted-4.jpg" },
+    {title: "Grand Theft Auto V", price: 39.99, platforms: ["PS5", "PS4", "PC", "XBOX Series X", "XBOX One"], metacriticScore: 97, genres: ["Action", "Adventure"], publisher: "Rockstar Games", developer: "Rockstar North", releaseDate: "11/18/2014", imagePath: "./images/gta-v.jpg" },
+    {title: "Horizon Zero Dawn", price: 19.99, platforms: ["PS4", "PC"], metacriticScore: 89, genres: ["Action", "RPG"], publisher: "Sony Interactive Entertainment", developer: "Guerrilla", releaseDate: "02/28/2017", imagePath: "./images/horizon-zero-dawn.jpg" },
+]
+
+
+
